4dda0f5ad05ada1909cf86a5de2e577784e552f6
ZacSmith0622 <39052417+ZacSmith0622@users.noreply.github.com>
2018-08-01 13:04:03 -0400

    Changes that I've made this week
    Added the nav bar, with icons linking to the weather page. Removed the time clock at the top, removed the map and weather at the bottom, and changed some of the color theme around.

--- calendar-team-two-frontend-master/main.js
+++ calendar-team-two-frontend-master/main.js
@@ -1,38 +1,139 @@
+const ENTER_KEY = 13;
+
 let weeks = [];
 let month = moment().month() + 1; //months are zero-indexed
 let year = moment().year();
 let today = moment().date();
 let events = [];
-const ENTER_KEY = 13;
+let client;
+let db;
+let credential;
+let username = '';
+let password = '';
+let alarmTimes = [];
 
-var alarm = new Audio("http://soundbible.com/grab.php?id=2197&type=mp3");
-var input = document.getElementById("textInput");
+let alarm = new Audio("http://soundbible.com/grab.php?id=2197&type=mp3");
+let input = document.getElementById("textInput");
+
+let jsalarm = {};
 
 window.addEventListener('load', load, false);
-$(document).ready(function(){
+$(document).ready(function() {
     $('select').formSelect();
     $('option').formSelect();
 });
 
-function load() {
-    let input = document.getElementById("textInput");
+function tryLoginFromLocalStorage() {
+    client = stitch.Stitch.initializeDefaultAppClient('calendar-urrdo');
 
-    input.addEventListener("keydown", function (event) {
+    username = localStorage.getItem("username-calendar");
+    password = localStorage.getItem("password-calendar");
+
+    if (username != '' && password != '') {
+        credential = new stitch.UserPasswordCredential(username, password);
+        db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('Calendar');
+
+        loadEvents();
+    }
+
+}
+
+function load() {
+    moment.modifyHolidays.add({
+        "Appiah's Birthday": {
+            date: '10/06',
+        },
+        "John's Birthday": {
+            date: '10/09',
+        },
+        "Olga's Birthday": {
+            date: '08/28',
+        },
+        "Webster's Birthday": {
+            date: '08/03',
+        },
+        "Linda's Birthday": {
+            date: '05/29',
+        }
+    });
+
+    let input = document.getElementById("textInput");
+    let loginButton = document.getElementById("loginLink");
+    let logoutButton = document.getElementById("logoutLink");
+
+    tryLoginFromLocalStorage();
+
+    logoutButton.onclick = function() {
+        logoutButton.style.visibility = "hidden";
+        loginButton.style.visibility = "visible";
+
+        username = '';
+        password = '';
+        credential = undefined;
+        db = undefined;
+        events = [];
+        localStorage.setItem("username-calendar", '');
+        localStorage.setItem("password-calendar", '');
+
+        loadDates();
+    }
+
+    loginButton.onclick = function() {
+        let thePrompt = window.open("", "", "height=150,width=300");
+        let theHTML = "";
+
+        theHTML += "Username: <input type='text' id='theUser' placeholder='Enter Username'/>";
+        theHTML += "<br />";
+        theHTML += "Password: <input type='password' id='thePass' placeholder='Enter Password'/>";
+        theHTML += "<br />";
+        theHTML += "<input type='button' value='Login' id='authOK'/>";
+        thePrompt.document.body.innerHTML = theHTML;
+
+        thePrompt.document.getElementById("thePass").addEventListener("keydown", function(event) {
+            if (event.keyCode === ENTER_KEY) {
+                submitPassword();
+            }
+        });
+
+        thePrompt.document.getElementById("authOK").onclick = function() {
+            submitPassword();
+        }
+
+        function submitPassword() {
+            username = thePrompt.document.getElementById("theUser").value;
+            password = thePrompt.document.getElementById("thePass").value;
+
+            credential = new stitch.UserPasswordCredential(username, password);
+            localStorage.setItem("username-calendar", username);
+            localStorage.setItem("password-calendar", password);
+
+            db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('Calendar');
+
+            thePrompt.close();
+            loadEvents();
+        }
+    }
+
+    input.addEventListener("keydown", function(event) {
         if (event.keyCode === ENTER_KEY) {
             create();
         }
     });
-    
+
     //http://www.javascriptkit.com/script/script2/alarm.shtml
-    var jsalarm = {
+    jsalarm = {
         padfield: function(f) {
             return (f < 10) ? "0" + f : f
         },
         showcurrenttime: function() {
             var dateobj = new Date()
+
+            this.findClosestTime();
+
             var ct = this.padfield(dateobj.getHours()) + ":" + this.padfield(dateobj.getMinutes()) + ":" + this.padfield(dateobj.getSeconds())
             this.ctref.innerHTML = ct
             this.ctref.setAttribute("title", ct)
+
             if (typeof this.hourwake != "undefined") { //if alarm is set
                 if (this.ctref.title == (this.hourwake + ":" + this.minutewake + ":" + this.secondwake)) {
                     clearInterval(jsalarm.timer)
@@ -40,6 +141,55 @@ function load() {
                     alarm.play()
                     alert("Check Your Calendar")
                 }
+            }
+        },
+        findClosestTime: function() {
+            let currentTime = moment();
+
+            let validAlarmTimes = [];
+
+            for (let e of alarmTimes) {
+                if (!moment(e, ["h:mm A"]).isValid()) {
+                    continue;
+                }
+                let alarmHour = moment(e, ["h:mm A"]).format("H");
+                let alarmMinutes = moment(e, ["h:mm A"]).format("m");
+
+                if (alarmHour > currentTime.format("H")) {
+                    validAlarmTimes.push(e);
+                } else if (alarmHour == currentTime.format("H") &&
+                    parseInt(alarmMinutes) > parseInt(currentTime.format("m"))) {
+                    validAlarmTimes.push(e);
+                }
+            }
+
+            let minElement = undefined;
+            let minHour = 0;
+            let minMinutes = 0;
+
+            for (let i = 0; i < validAlarmTimes.length; i++) {
+                let alarmHour = moment(validAlarmTimes[i], ["h:mm A"]).format("HH");
+                let alarmMinutes = moment(validAlarmTimes[i], ["h:mm A"]).format("m");
+
+                if (i === 0) {
+                    minElement = validAlarmTimes[i];
+                    minHour = alarmHour;
+                    minMinutes = minMinutes;
+                } else {
+                    if (alarmHour < minHour) {
+                        minElement = validAlarmTimes[i];
+                        minHour = alarmHour;
+                        minMinutes = minMinutes;
+                    } else if (alarmHour == minHour && alarmMinutes < minMinutes) {
+                        minElement = validAlarmTimes[i];
+                        minHour = alarmHour;
+                        minMinutes = minMinutes;
+                    }
+                }
+            }
+
+            if (minElement != undefined) {
+                this.setEventAlarm(minElement);
             }
         },
         init: function() {
@@ -96,21 +246,37 @@ function load() {
             this.minuteselect.disabled = true
             this.secondselect.disabled = true
         },
+        setEventAlarm: function(alarmTime) {
+            this.hourwake = moment(alarmTime, ["h:mm A"]).format("HH")
+            this.minutewake = moment(alarmTime, ["h:mm A"]).format("mm")
+            this.secondwake = moment(alarmTime, ["h:mm A"]).format("ss")
+            this.hourselect.disabled = true
+            this.minuteselect.disabled = true
+            this.secondselect.disabled = true
+        }
     }
-    
+
     jsalarm.init();
 }
 
 function deleteEvents() {
-	events = [];
-	loadDates();
+    if (credential == undefined) {
+        return;
+    }
+
+    events = [];
+    alarmTimes = [];
+    client.auth.loginWithCredential(credential).then(() => db.collection('Events').deleteMany()).then(() =>
+        loadEvents()
+    );
+
 }
 
 function resetToPresent() {
     month = moment().month() + 1;
     year = moment().year();
 
-    loadDates();
+    loadEvents();
 }
 
 function myMap() {
@@ -121,50 +287,38 @@ function myMap() {
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
-//https://gist.github.com/jrhames/5200024
-(function () {
-    var moment;
+function loadEvents() {
+    let loginButton = document.getElementById("loginLink");
+    let logoutButton = document.getElementById("logoutLink");
 
-    moment = typeof require !== "undefined" && require !== null ? require("moment") : this.moment;
-
-    //Holiday definitions
-    var _holidays = {
-        'M': { //Month, Day
-            '01/01': "New Year's Day",
-            '07/04': "Independence Day",
-            '11/11': "Veteran's Day",
-            '12/24': "Christmas Eve",
-            '12/25': "Christmas Day",
-            '12/31': "New Year's Eve",
-            '10/06': "Appiah's Birthday",
-            '10/09': "John's Birthday",
-            '08/28': "Olga's Birthday",
-            '08/03': "Webster's Birthday",
-            '05/29': "Linda's Birthday"
-        },
-        'W': { //Month, Week of Month, Day of Week
-            '1/3/1': "Martin Luther King Jr. Day",
-            '2/3/1': "Washington's Birthday",
-            '5/5/1': "Memorial Day",
-            '9/1/1': "Labor Day",
-            '10/2/1': "Columbus Day",
-            '11/4/4': "Thanksgiving Day",
-            '6/3/0': "Father's Day",
-            "5/2/0": "Mother's Day"
-        }
-    };
-
-    moment.fn.holiday = function () {
-        var diff = 1 + (0 | (this._d.getDate() - 1) / 7),
-            memorial = (this._d.getDay() === 1 && (this._d.getDate() + 7) > 30) ? "5" : null;
-
-        return (_holidays['M'][this.format('MM/DD')] || _holidays['W'][this.format('M/' + (memorial || diff) + '/d')]);
-    };
-
-    if ((typeof module !== "undefined" && module !== null ? module.exports : void 0) != null) {
-        module.exports = moment;
+    if (credential == undefined) {
+        loadDates();
+        return;
     }
-}(this));
+
+    client.auth.loginWithCredential(credential).then(() => db.collection('Events').find({ owner_id: client.auth.user.id }).asArray())
+        .then(docs => {
+            events = [];
+
+            for (let doc of docs) {
+                events.push(doc.event);
+            }
+
+            loadDates();
+
+            loginButton.style.visibility = "hidden";
+            logoutButton.style.visibility = "visible";
+
+        }).catch(err => {
+            events = [];
+            loadDates();
+
+            loginButton.style.visibility = "visible";
+            logoutButton.style.visibility = "hidden";
+            console.error(err)
+        });
+
+}
 
 function loadDates() {
     if (month == moment().month() + 1 && year == moment().year()) {
@@ -198,12 +352,12 @@ function loadDates() {
             let todaysDate = "" + moment().year() + "-" + (moment().month() + 1) + "-" + moment().date();
 
             if (weeks[i][j].unix() == moment(todaysDate, "YYYY-MM-DD").unix()) {
-                li.style.backgroundColor = "lightgreen";
+                li.style.backgroundColor = "#6D7993";
                 span.innerHTML = "Today <br />";
             }
 
-            if (moment(weeks[i][j]).holiday() != undefined) {
-                span.innerHTML = moment(weeks[i][j]).holiday() + "<br />";
+            if (moment(weeks[i][j]).isHoliday()) {
+                span.innerHTML = moment(weeks[i][j]).isHoliday() + "<br />";
             }
 
             li.innerHTML = weeks[i][j].format('D') + "<br />"; //example: innerHTML = 31
@@ -214,9 +368,15 @@ function loadDates() {
             //adds the events to the date
             for (let i = 0; i < events.length; i++) {
                 if (span.value.format("MMMM DD YYYY") == moment(events[i].date).format("MMMM DD YYYY")) {
+                    //getting the times for the alarm
+                    if (events[i].date == moment(todaysDate).format("MMMM DD YYYY")) {
+                        if (!alarmTimes.includes(events[i].time)) {
+                            alarmTimes.push(events[i].time);
+                        }
+                    }
+
                     span.id = i;
                     span.innerHTML = span.innerHTML + events[i].time + ": " + events[i].note + "<br />";
-                    //console.log(span.id);
                 }
             }
 
@@ -225,7 +385,7 @@ function loadDates() {
                 li.setAttribute("style", "color: grey; font-style: italic;");
             }
 
-            if (moment(weeks[i][j]).holiday() != undefined) {
+            if (moment(weeks[i][j]).isHoliday()) {
                 li.style.backgroundColor = "lightblue";
             }
 
@@ -235,38 +395,59 @@ function loadDates() {
         }
     }
 
-
 }
 
 function create(event) {
     let textInput = document.getElementById("textInput");
-    let timeInput = document.getElementById("timeInput");
+    let timeInput = document.getElementById("apptTime");
     let eventForm = document.getElementById("event-form");
-	
+
     if (textInput.value.length > 0) {
-        events.push({
-            date: eventForm.value,
-            time: timeInput.value,
-            note: textInput.value
-        });
-		console.log(events);
+        let textValue = textInput.value;
+        let timeValue = apptTime.value;
+
+        if (credential == undefined) {
+            resetForm();
+            return;
+        }
+
+        //console.log(eventForm.value.format("MMMM DD YYYY"));
+
+        client.auth.loginWithCredential(credential).then(() =>
+            db.collection('Events').insertOne({
+                owner_id: client.auth.user.id,
+                event: {
+                    date: eventForm.value.format("MMMM DD YYYY"),
+                    time: timeValue,
+                    note: textValue
+                }
+            }).then(() => {
+                loadEvents();
+                resetForm();
+            }).catch(err => {
+                console.error(err)
+            }));
     }
-    eventForm.classList.add("inactive");
-    eventForm.classList.remove("active");
-    textInput.value = "";
-    timeInput.value = "";
-    loadDates();
+
+    function resetForm() {
+        eventForm.classList.add("inactive");
+        eventForm.classList.remove("active");
+        textInput.value = "";
+        apptTime.value = "";
+    }
+
 }
 
 function clickedBox(event) {
-
     let eventForm = document.getElementById("event-form");
     eventForm.classList.remove("inactive");
     eventForm.classList.add("active");
-    eventForm.style = "top: " + event.clientY + "; left: " + event.clientX;
+    eventForm.style = "top: 75%; left: 50%";
+
+    //eventForm.style = "top: " + event.clientY + "; left: " + event.clientX;
     let p = eventForm.getElementsByTagName("p")[0];
     let span = event.target.getElementsByTagName("span")[0];
-    document.getElementById("timeInput").select();
+    document.getElementById("apptTime").select();
 
     p.innerHTML = span.value.format('MMMM DD YYYY');
     eventForm.value = span.value;
@@ -342,7 +523,7 @@ function incrementMonth() {
         month += 1;
     }
 
-    loadDates();
+    loadEvents();
 }
 
 function decrementMonth() {
@@ -353,17 +534,17 @@ function decrementMonth() {
         month -= 1;
     }
 
-    loadDates();
+    loadEvents();
 }
 
 function incrementYear() {
     year++;
 
-    loadDates();
+    loadEvents();
 }
 
 function decrementYear() {
     year--;
 
-    loadDates();
+    loadEvents();
 }
